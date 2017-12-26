4b01178add41a97a0daafc5c7e958a02466fb965
Hayssam Saleh <hayssam.saleh@ebiznext.com>
2017-12-26 11:30:13 +0200

    WIP: Migration to AKKA-HTTP

--- src/main/scala/com/mogobiz/system/MogobizSystem.scala
+++ src/main/scala/com/mogobiz/system/MogobizSystem.scala
@@ -4,15 +4,12 @@
 
 package com.mogobiz.system
 
-import akka.actor.{Actor, ActorLogging, ActorSystem}
+import akka.actor.ActorSystem
 import akka.event.Logging._
-import spray.http.StatusCodes._
-import spray.http._
-import spray.routing._
-import spray.routing.directives.LogEntry
-import spray.util.LoggingContext
-
-import scala.util.control.NonFatal
+import akka.http.scaladsl.model.{HttpRequest, HttpResponse}
+import akka.http.scaladsl.server.directives.LogEntry
+import akka.http.scaladsl.unmarshalling.Unmarshal
+import akka.stream.ActorMaterializer
 
 /**
   * Core is type containing the ``system: ActorSystem`` member. This enables us to use it in our
@@ -20,21 +17,8 @@ import scala.util.control.NonFatal
   */
 trait MogobizSystem {
   implicit def system: ActorSystem
-
+  implicit val materializer = ActorMaterializer()
   //  def breaker: CircuitBreaker
-
-  def debugRequest(request: HttpRequest): LogEntry = {
-    val s = request.headers.map { header =>
-      "Header : " + header.name + " =>" + header.value
-    }.mkString("\n") + "ENTITY\n" + request.entity.asString
-    LogEntry(s, InfoLevel)
-  }
-
-  def showRequest(request: HttpRequest): HttpResponsePart ⇒ Option[LogEntry] = {
-    case HttpResponse(s, _, _, _)                        ⇒ Some(LogEntry(s"${s.intValue}: ${request.uri}", InfoLevel))
-    case ChunkedResponseStart(HttpResponse(OK, _, _, _)) ⇒ Some(LogEntry(" 200 (chunked): ${request.uri}", InfoLevel))
-    case _                                               ⇒ None
-  }
 }
 
 /**
@@ -55,42 +39,5 @@ trait BootedMogobizSystem extends MogobizSystem {
   /**
     * Ensure that the constructed ActorSystem is shut down when the JVM shuts down
     */
-  sys.addShutdownHook(system.shutdown())
-}
-
-/**
-  * @param responseStatus
-  * @param response
-  */
-case class ErrorResponseException(responseStatus: StatusCode, response: Option[HttpEntity]) extends Exception
-
-/**
-  * Allows you to construct Spray ``HttpService`` from a concatenation of routes; and wires in the error handler.
-  * It also logs all internal server errors using ``SprayActorLogging``.
-  *
-  * @param route the (concatenated) route
-  */
-class RoutedHttpService(route: Route) extends Actor with HttpService with ActorLogging {
-
-  implicit def actorRefFactory = context
-
-  implicit val handler = ExceptionHandler {
-    case NonFatal(ErrorResponseException(statusCode, entity)) =>
-      ctx =>
-        ctx.complete(statusCode, entity)
-
-      case NonFatal(e) =>
-      ctx =>
-        {
-          log.error(e, InternalServerError.defaultMessage)
-          ctx.complete(InternalServerError)
-        }
-  }
-
-  def receive: Receive =
-    runRoute(route)(handler,
-                    RejectionHandler.Default,
-                    context,
-                    RoutingSettings.default,
-                    LoggingContext.fromActorRefFactory)
+  sys.addShutdownHook(system.terminate())
 }
